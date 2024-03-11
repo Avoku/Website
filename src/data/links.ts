328e47bc092c90466b567cc0a90f26f20666d011
Naibuu <hawkwer141099@gmail.com>
2024-03-11 20:55:16 +0200

    add: Temporary navbar

---
+++ src/data/links.ts
@@ -0,0 +1,27 @@
+interface Link {
+    text: string
+    href: string
+}
+
+interface Social {
+    // must be in lowercase and valid.
+    name: string
+    href: string
+}
+
+export const Links: Link[] = [
+    {
+        text: "Home",
+        href: "/"
+    },
+    {
+        text: "Home",
+        href: "/"
+    },
+    {
+        text: "Home",
+        href: "/"
+    }
+]
+
+export const Socials: Social[] = []
