c6778800b91f669bd36f11e21b7e4d84cbe2ba94
Naibuu <hawkwer141099@gmail.com>
2024-03-14 22:19:53 +0200

    add: cards + rewrites (needs improvements)

--- astro.config.ts
+++ astro.config.ts
@@ -5,4 +5,9 @@ import sitemap from '@astrojs/sitemap'
 export default defineConfig({
     site: 'https://avoku.github.io',
     integrations: [sitemap()],
+    redirects: {
+        '/github': 'https://github.com/Avoku',
+        '/discord': 'https://discord.gg/jpsXV4baWA',
+        '/rbot': 'https://polymart.org/resource/discord-ranked-bot.3636',
+    }
 })
